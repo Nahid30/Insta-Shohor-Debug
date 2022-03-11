081d1a2125c13b8ee676e804db3c30ff8a8ef00a
Nahid30 <nahidshuvo30@gmail.com>
2022-03-11 18:35:11 +0600

    like repeated problem solved again beacuse the older solved was commented which i did not noticed

--- js/app.js
+++ js/app.js
@@ -144,7 +144,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-  // document.getElementById("liked").innerHTML='';
+  document.getElementById("liked").innerHTML='';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -154,7 +154,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
